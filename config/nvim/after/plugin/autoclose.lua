43a08870354e3c17fe0ce75bd1b3ffaae2a36f71
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2026-03-09 14:26:52 -0400

    gug

---
+++ config/nvim/after/plugin/autoclose.lua
@@ -0,0 +1,10 @@
+require('autoclose').setup({
+	keys = {
+		["("] = { escape = false, close = false, pair = "()", disabled_filetypes = {} },
+		["["] = { escape = false, close = false, pair = "[]", disabled_filetypes = {} },
+		["{"] = { escape = false, close = false, pair = "{}", disabled_filetypes = {} },
+		["'"] = { escape = false, close = false, pair = "''", disabled_filetypes = {} },
+		['"'] = { escape = false, close = false, pair = '""', disabled_filetypes = {} },
+	}
+})
+

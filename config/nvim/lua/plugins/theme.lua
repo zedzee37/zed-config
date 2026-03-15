c224a0f3756e8aa85c66e85ed4eb02a39b001587
zedzee <carsonuderitz@gmail.com>
2026-03-15 11:44:41 -0400

    jetbrain theem

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,25 +1,35 @@
+-- return {
+--     "tiagovla/tokyodark.nvim",
+--     opts = {
+--         -- custom options here
+--     },
+--     config = function(_, opts)
+--         require("tokyodark").setup({
+-- 			transparent_background = true,
+-- 			styles = {
+-- 				comments = { italic = false }, -- style for comments
+-- 				keywords = { italic = false }, -- style for keywords
+-- 				identifiers = { italic = false }, -- style for identifiers
+-- 				functions = { italic = false }, -- style for functions
+-- 				variables = { italic = false }, -- style for variables
+-- 			},
+-- 			custom_highlights = function(highlights, palette)
+-- 				return {
+-- 					Identifier = { fg = palette.fg },
+-- 					Constant = { fg = palette.fg }
+-- 				}
+-- 			end
+-- 		}) -- calling setup is optional
+--         vim.cmd [[colorscheme tokyodark]]
+--     end,
+-- }
 return {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup({
-			transparent_background = true,
-			styles = {
-				comments = { italic = false }, -- style for comments
-				keywords = { italic = false }, -- style for keywords
-				identifiers = { italic = false }, -- style for identifiers
-				functions = { italic = false }, -- style for functions
-				variables = { italic = false }, -- style for variables
-			},
-			custom_highlights = function(highlights, palette)
-				return {
-					Identifier = { fg = palette.fg },
-					Constant = { fg = palette.fg }
-				}
-			end
-		}) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
+    "nickkadutskyi/jb.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        require("jb").setup({transparent = true})
+        vim.cmd("colorscheme jb")
     end,
 }

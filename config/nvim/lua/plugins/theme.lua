09a0febcf6839ddb80554ce1b46db7b1eeddb429
zedzee <carsonuderitz@gmail.com>
2026-04-19 21:36:09 -0400

    go back to jb

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,36 +1,36 @@
-return {
-    "tiagovla/tokyodark.nvim",
-    opts = {
-        -- custom options here
-    },
-    config = function(_, opts)
-        require("tokyodark").setup({
-			transparent_background = true,
-			styles = {
-				comments = { italic = false }, -- style for comments
-				keywords = { italic = false, }, -- style for keywords
-				identifiers = { italic = false }, -- style for identifiers
-				functions = { italic = false }, -- style for functions
-				variables = { italic = false }, -- style for variables
-			},
-			custom_highlights = function(highlights, palette)
-				return {
-					Identifier = { fg = palette.fg },
-					Constant = { fg = palette.fg }
-				}
-			end
-		}) -- calling setup is optional
-        vim.cmd [[colorscheme tokyodark]]
-    end,
-}
 -- return {
---     "nickkadutskyi/jb.nvim",
---     lazy = false,
---     priority = 1000,
---     opts = {},
---     config = function()
---         require("jb").setup({transparent = true})
---         vim.cmd("colorscheme jb")
+--     "tiagovla/tokyodark.nvim",
+--     opts = {
+--         -- custom options here
+--     },
+--     config = function(_, opts)
+--         require("tokyodark").setup({
+-- 			transparent_background = true,
+-- 			styles = {
+-- 				comments = { italic = false }, -- style for comments
+-- 				keywords = { italic = false, }, -- style for keywords
+-- 				identifiers = { italic = false }, -- style for identifiers
+-- 				functions = { italic = false }, -- style for functions
+-- 				variables = { italic = false }, -- style for variables
+-- 			},
+-- 			custom_highlights = function(highlights, palette)
+-- 				return {
+-- 					Identifier = { fg = palette.fg },
+-- 					Constant = { fg = palette.fg }
+-- 				}
+-- 			end
+-- 		}) -- calling setup is optional
+--         vim.cmd [[colorscheme tokyodark]]
 --     end,
 -- }
+return {
+    "nickkadutskyi/jb.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        require("jb").setup({transparent = true})
+        vim.cmd("colorscheme jb")
+    end,
+}
 

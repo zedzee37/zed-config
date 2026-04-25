01f71b6f150be1d5672fb68ddeaea24a0266b31b
zedzee <carsonuderitz@gmail.com>
2026-04-25 03:39:21 -0400

    switch to oxocarbon

---
+++ config/nvim/after/plugin/theme.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
+
+vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopePromptTitle', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopePromptBorder', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopePreviewTitle', {bg='none'})
+vim.api.nvim_set_hl(0, 'TelescopeResultsTitle', {bg='none'})
--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -23,14 +23,93 @@
 --         vim.cmd [[colorscheme tokyodark]]
 --     end,
 -- }
-return {
-    "nickkadutskyi/jb.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        require("jb").setup({transparent = true})
-        vim.cmd("colorscheme jb")
-    end,
-}
+-- return {
+--   'thimc/gruber-darker.nvim',
+--   config = function()
+--     require('gruber-darker').setup({
+--       -- OPTIONAL
+--       transparent = true, -- removes the background
+--       -- underline = false, -- disables underline fonts
+--       -- bold = false, -- disables bold fonts
+--     })
+--     vim.cmd.colorscheme('gruber-darker')
+--   end,
+-- }
+-- return {
+-- 	'Yazeed1s/minimal.nvim',
+-- 	config = function()
+-- 		vim.cmd.colorscheme("minimal")
+-- 		vim.g.minimal_transparent_background = true
+-- 	end
+-- }
 
+
+-- return {
+--     "nickkadutskyi/jb.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         require("jb").setup({transparent = true})
+--         vim.cmd("colorscheme jb")
+--     end,
+-- }
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		vim.o.background = "dark"
+-- 		vim.cmd([[colorscheme gruvbox]])
+-- 		require("gruvbox").setup({
+-- 			transparent_mode = true,
+-- 		})
+-- 	end
+-- }
+--
+-- return {
+-- 	"blazkowolf/gruber-darker.nvim",
+-- 	config = function()
+-- 		vim.cmd.colorscheme("gruber-darker")
+-- 	end
+-- }
+-- return {
+--     'behemothbucket/gruber-darker-theme.nvim',
+--     priority = 1000,
+--     lazy = false,
+--     config = function()
+--         require('gruber-darker').setup()
+--         vim.cmd('colorscheme gruber-darker')
+--     end,
+-- }
+-- return {
+--     "Mofiqul/adwaita.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     
+--     config = function()
+--         vim.g.adwaita_disable_cursorline = true 
+--         vim.g.adwaita_transparent = true        
+--         vim.cmd('colorscheme adwaita')
+--     end
+-- }
+-- return {
+-- 	"Shatur/neovim-ayu",
+-- 	config = function()
+-- 		require("ayu").setup({
+-- 			overrides = {
+-- 					
+-- 			}
+-- 		})
+-- 		vim.cmd.colorscheme("ayu")
+-- 	end
+-- }
+--
+return {
+	"nyoom-engineering/oxocarbon.nvim",
+	config = function()
+		vim.cmd.colorscheme("oxocarbon")
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+	end
+}

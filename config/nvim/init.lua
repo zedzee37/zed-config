dab7b62c8875bec755ef0ca53ff6e99cb2f2dd7b
zedzee <zedzee37@proton.me>
2026-06-02 17:59:54 -0400

    hgihtlihgt on yank

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -67,3 +67,9 @@ vim.lsp.config("basedpyright", {
 		},
 	},
 })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function() vim.hl.on_yank() end,
+})
